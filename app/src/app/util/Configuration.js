4f15acfca33035303f413391c73ec5ec24d01b38
Nils Weiher <weiher@karazy.net>
2012-09-17 14:47:19 +0200

    adjusted ping interval

--- app/src/app/util/Configuration.js
+++ app/src/app/util/Configuration.js
@@ -13,8 +13,8 @@ Ext.define('EatSense.util.Configuration', {
 		channelReconnectTries: 20,
 		heartbeatInterval: 10000,
 		// Interval for channel ping messages.
-		channelPingInterval: 31000,
+		channelPingInterval: 120000,
 		// Amount of time in ms to wait for a channel message after ping.
-		channelMessageTimeout: 30000
+		channelMessageTimeout: 40000
 	}
 });
