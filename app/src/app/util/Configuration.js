4fda333ee435858165c3944f5dac77ade4780ff9
Frederik Reifschneider <reifschneider@karazy.net>
2012-10-25 15:32:14 +0200

    new git version

--- app/src/app/util/Configuration.js
+++ app/src/app/util/Configuration.js
@@ -6,7 +6,7 @@ Ext.define('EatSense.util.Configuration', {
 		msgboxHideLongTimeout: 1500,
 		//default format is EUR, can be changed during runtime depending on business
 		currencyFormat: 'EUR',
-		version: 'v1.4-63-g3fc9743',
+		version: 'v1.4-64-g90deb39',
 		//true means caching is disabled
 		disableCaching: false,
 		//contains the language during runtime used througout the application
