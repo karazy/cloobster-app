b17f69906d9e9f02571410938e49e28c681a8c18
Frederik Reifschneider <reifschneider@karazy.net>
2012-11-09 15:33:08 +0100

    removed old code base

--- app/src_old/app/store/History.js
+++
@@ -1,9 +0,0 @@
-Ext.define('EatSense.store.History', {
-	extend: 'Ext.data.Store',
-	requires: ['EatSense.model.History'],
-	config: {
-		storeId: 'historyStore',
-		model: 'EatSense.model.History',
-		pageSize: 15	
-	}
-})
--- app/src_old/app/store/Menu.js
+++
@@ -1,8 +0,0 @@
-Ext.define('EatSense.store.Menu', {
-	extend: 'Ext.data.Store',
-	requires: ['EatSense.model.Menu'],
-	config: {		
-		model: 'EatSense.model.Menu',		
-		storeId: 'menuStore'
-	}
-});
