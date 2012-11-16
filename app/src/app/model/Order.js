0ea7f2ee8ef2a30fa60a441662626925da8e5645
Frederik Reifschneider <reifschneider@karazy.net>
2012-11-16 09:37:29 +0100

    removed check_id which I introduced during tests for a bugfix

--- app/src/app/model/Order.js
+++ app/src/app/model/Order.js
@@ -59,10 +59,7 @@ Ext.define('EatSense.model.Order', {
 		proxy: {
 			type: 'rest',
 			enablePagingParams: false,
-			url : '/c/businesses/{pathId}/orders',
-			// reader: {
-			// 	type: 'json'
-		 //   	}
+			url : '/c/businesses/{pathId}/orders'
 	 	},
 	 	// current state of this order. used for store and restore
 	 	state: null
@@ -163,7 +160,7 @@ Ext.define('EatSense.model.Order', {
 		rawJson.productName = this.get('productName');
 		rawJson.productId = this.get('productId');
 		rawJson.productPrice = this.get('productPrice');
-		rawJson.checkin_id = this.get('checkin_id');
+		// rawJson.checkin_id = this.get('checkin_id');
 		rawJson.productShortDesc = this.get('productShortDesc');
 		rawJson.productLongDesc = this.get('productLongDesc');
 		rawJson.orderTime = (this.get('orderTime')) ? this.get('orderTime').getTime() : null;
@@ -217,7 +214,7 @@ Ext.define('EatSense.model.Order', {
 		this.set('productPrice', rawData.productPrice);
 		this.set('productShortDesc', rawData.productShortDesc);
 		this.set('productLongDesc', rawData.productLongDesc);
-		this.set('checkin_id', rawData.checkin_id);
+		// this.set('checkin_id', rawData.checkin_id);
 
 		return true;	
 	},
