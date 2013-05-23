5e87c8c4192035f918a1ba6c2e9d120e30902871
Frederik Reifschneider <frederik.reifschneider@gmail.com>
2013-05-23 17:49:01 +0200

    added missing file

---
+++ app/src/app/util/None.js
@@ -0,0 +1,25 @@
+/**
+ * @author Frederik Reifschneider
+ *
+ * This class prevents sencha touch 2 from generating custom ids.
+ *
+ */
+Ext.define('EatSense.util.None', {
+    alias: 'data.identifier.none',
+    
+    statics: {
+        // AUTO_ID: 1
+    },
+
+    config: {
+        // prefix: 'ext-record-'
+    },
+
+    constructor: function(config) {
+        this.initConfig(config);
+    },
+
+    generate: function(record) {
+        return "";
+    }
+});
