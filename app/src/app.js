192a6bbf66ec0d2d036a29a5a25ee1a4784260a1
Frederik Reifschneider <frederik.reifschneider@gmail.com>
2013-08-20 11:46:03 +0200

    remove sync configuration for whitelabel call

--- app/src/app.js
+++ app/src/app.js
@@ -335,7 +335,6 @@ Ext.application({
 
       Ext.Ajax.request({
         url: 'whitelabel/' + configName + '/Configuration.json',
-        synchronous: true,
         success: function(response) {
           console.log('Application.initWhitelabelConfiguration: found whitelabel configuration for ' + whitelabelConfig);
           try {
